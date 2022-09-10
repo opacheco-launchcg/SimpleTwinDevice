fd1264769397790168f45f53da0536e63eaac0e6
OPacheco <OPacheco@launchcg.com>
2022-09-09 21:13:04 -0300

    Adding logic to process csv telemetry data in python script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SimpleTwinDevice  
+# create-twin-devices  
 Powershell script to create and update twin devices in an Azure IoT Hub  
 
 - Prerequisites: Install the following in your local computer  
@@ -6,12 +6,28 @@ Powershell script to create and update twin devices in an Azure IoT Hub
 https://docs.microsoft.com/en-us/cli/azure/ - Azure CLI  
 https://dotnet.microsoft.com/en-us/download/dotnet/3.1 - .NET SDK 3.1.28  
 
-- To use the scrpit, simply use the powershell command (all params are optional):  
+- To use the script, simply use the powershell command (all params are optional):  
 
 create-twin-devices.ps1 -addTags [1/true, 0/false] -addParams [1/true, 0/false] -createAll [1/true, 0/false]  
 
 -addTags: If true, will create/update twin devices Tags  
 -addParams: If true, will create/update twin devices Desired Properties  
 -createAll: If true, will attempt to create Resource Group, IoT Hub, Parent Device and Twin Devices  
+
+# update-reported-props
+Python script to process telemetry data and send it as reported properties  
+
+- Prerequisites: Install the following in your local computer  
+
+https://www.python.org/downloads/ - Python  
+Once Python is installed run the following command  
+
+pip install azure-iot-device
+
+- To use the script, run cmd. Navigate to the folder containing the script and run  
+
+python update-reported-props <deviceName>
+
+-deviceName: The name of the device which will be updated. The connection string to this device must be specified in the json file
 
 
--- update-reported-props.py
+++ update-reported-props.py
@@ -1,29 +1,37 @@
 import os
+import sys
 import asyncio
+import json
+import csv
 import random
 from azure.iot.device.aio import IoTHubDeviceClient
 
+async def main():   
+    with open('.\\update-reported-props.json') as json_data_file:
+        data = json.load(json_data_file)
 
-async def main():
-    conn_str = "HostName=TestParentDevice.azure-devices.net;DeviceId=TM004;SharedAccessKey=cpnYb+d7Tp8wXBASVR84BFIdHBDA5LIzgZbqCzl1dgQ="
+    # get connection string from config using device name as param
+    conn_str = data['connStrings'][sys.argv[1]]
     device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
 
     # connect the client.
     await device_client.connect()
 
-    # update the reported properties
-    reported_properties = {"wind_velocity": random.randint(320, 800) / 10}
-    print("Setting reported temperature to {}".format(reported_properties["wind_velocity"]))
-    await device_client.patch_twin_reported_properties(reported_properties)
+    # open data telemetry file
+    reported_properties = {};
+    with open('.\\sample_telemetry.csv', mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if row[0] == sys.argv[1]:
+                reported_properties = {};
+                for idx in data['colNames']:
+                    reported_properties = {data['colNames'][idx] : row[int(idx)] }
+
+                    # update the reported properties
+                    await device_client.patch_twin_reported_properties(reported_properties)
 
     # Finally, shut down the client
     await device_client.shutdown()
 
-
 if __name__ == "__main__":
     asyncio.run(main())
-
-    # If using Python 3.6 use the following code instead of asyncio.run(main()):
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main())
-    # loop.close()
